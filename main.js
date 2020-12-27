71c1204ae1f76ed090f9c843852525131775fcab
kooo8282 <kooo8282@gmail.com>
2020-12-28 01:32:53 +0900

    refactoring by function

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, tags, body){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB2</a></h1>
+    ${tags}
+    ${body}
+    </body>
+    </html>
+    `;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     let parsedURL = url.parse(_url, true);
@@ -11,56 +28,30 @@ var app = http.createServer(function(request,response){
     
     if(parsedURL.pathname === '/'){
         if(queryData.id === undefined){
-            let tags = '<ul>';
             fs.readdir('data',function(err,filelist){
+                let tags = '<ul>';
                 for(let i=0; i<filelist.length; i++){
                     tags += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
                 }
                 tags += '</ul>';
                 let title = 'Welcome';
                 let description = 'Hello, Node.js';
-                let template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                ${tags}
-                <h2>${title}</h2>
-                <p>${description}</p>
-                </body>
-                </html>
-                `;
+                let body = `<h2>${title}</h2><p>${description}</p>`;
+                let template = templateHTML(title, tags, body);
                 response.writeHead(200);
                 response.end(template);  
             });                      
         } else{
-            let tags = '<ul>';
             fs.readdir('data',function(err,filelist){
+                let tags = '<ul>';
                 for(let i=0; i<filelist.length; i++){
                     tags += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
                 }
                 tags += '</ul>';
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){        
                     let title = queryData.id;
-                    let template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${tags}                
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>
-                    `;
+                    let body = `<h2>${title}</h2><p>${description}</p>`;
+                    let template = templateHTML(title, tags, body);
                     response.writeHead(200);
                     response.end(template);
                 });
